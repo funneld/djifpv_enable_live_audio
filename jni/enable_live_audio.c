c4370b0223e29983544f3b32a05d22ffea76ac6f
funneld <funneld@gmail.com>
2022-07-08 08:32:40 +0200

    replace sleep with timer

--- jni/enable_live_audio.c
+++ jni/enable_live_audio.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <string.h>
 #include "enable_live_audio.h"
 
 
@@ -20,12 +21,12 @@ int32_t setAudioLiveViewEnable (void* this, bool param_1, bool param_2) {
 }
 
 
-int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {	
-		
+int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
+
 	int32_t ret = 0;
-	
+
 	if (!timeOut && !getInstance){
-		
+
 		timeOut = dlsym (RTLD_NEXT, "_ZN19GlassRacingChnlMenu7timeOutEv");
 		if (timeOut == 0){
 			guiLib = dlopen("/system/lib/libtp1801_gui.so", 1);
@@ -36,7 +37,7 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
 				return 0;
 			}
 		}
-		
+
 		getInstance = dlsym (RTLD_NEXT, "_ZN17GlassUserSettings11getInstanceEv");
 		if (getInstance == 0){
 			if(!guiLib){guiLib = dlopen("/system/lib/libtp1801_gui.so", 1);}
@@ -52,28 +53,34 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
 	uint32_t inst = getInstance();
 
 	gs_link_stat_t link = GS_LINK_STAT_UKNOWN;
-	gs_link_stat_t *link_state = &link;	
-	
+	gs_link_stat_t *link_state = &link;
+
 	uint32_t *gs_gui_config = (uint32_t *)*(uint32_t *)((int)inst + 0xe4);
 	uint32_t *gs_info = (uint32_t *)*(uint32_t *)((int)gs_gui_config + 0x4c);
 	gs_modem_get_link_state_wrap = (void *)*(uint32_t *)((int)gs_gui_config + 0x228); //fptr @gs_gui_config + 0x228
-	
+
 	gs_modem_get_link_state_wrap(gs_info, link_state);
-		
-	if(((link == GS_LINK_STAT_NORMAL) || (link == GS_LINK_STAT_WEAK)) && !restart_lv_audio){
+
+	if(link != GS_LINK_STAT_NORMAL){
+		clock_gettime(CLOCK_MONOTONIC, &start);
+	}
+
+	clock_gettime(CLOCK_MONOTONIC, &now);
+
+	if((link == GS_LINK_STAT_NORMAL) && !restart_lv_audio && ((now.tv_sec - start.tv_sec) > 9)){
 		sleep(10);	//wait for AU to boot
 		restart_lv_audio = true;
 		setAudioLiveViewEnable((void *)inst, true, false);
 		printf("setAudioLiveViewEnable ENABLED\n");
 	}
-	else if(((link == GS_LINK_STAT_LOST) || (link == GS_LINK_STAT_UKNOWN)) && restart_lv_audio){
+	else if((link == GS_LINK_STAT_LOST) && restart_lv_audio){
 		restart_lv_audio = false;
 		setAudioLiveViewEnable((void *)inst, false, false);
 		printf("setAudioLiveViewEnable DISABLED\n");
 	}
-	
 
-	ret = timeOut(this);		
 
-	return ret;	
+	ret = timeOut(this);
+
+	return ret;
 }
