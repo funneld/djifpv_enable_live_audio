88d11c5889b6abb33ce34b953bb39941455f593a
funneld <funneld@gmail.com>
2022-07-23 10:02:09 +0200

    check if AU-lite

--- jni/enable_live_audio.c
+++ jni/enable_live_audio.c
@@ -7,21 +7,21 @@
 #include "enable_live_audio.h"
 
 
-int32_t setAudioLiveViewEnable (void* this, bool param_1, bool param_2) {
+bool is_fpv_lite(void *gs_info){
+	char hw_ver[16];
+	memset(hw_ver, 0, 0x10);
 
-	uint32_t *gs_gui_config = (uint32_t *)*(uint32_t *)((int)this + 0xe4);
-	uint32_t *gs_info = (uint32_t *)*(uint32_t *)((int)gs_gui_config + 0x4c);
+	gs_get_uav_hardware_version(gs_info, hw_ver);	
+	
+	int fpv_lite = strncmp(hw_ver,"LT150 VT Ver.A",0x10);
+	
+	if(fpv_lite == 0)return true;
 
-	static uint32_t (*gs_enable_audio_liveview)(void *_gs_info, bool b_enable) = 0;
-	gs_enable_audio_liveview = (void *)*(uint32_t *)((int)gs_gui_config + 0x3e0);  //fptr @gs_gui_config + 0x3e0
-
-	gs_enable_audio_liveview(gs_info, param_1);
-
-	return 0;
+	return false;
 }
 
 
-int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
+int32_t _ZN19GlassRacingChnlMenu7timeOutEv(void* this){
 
 	int32_t ret = 0;
 
@@ -48,36 +48,42 @@ int32_t _ZN19GlassRacingChnlMenu7timeOutEv (void* this) {
 				return 0;
 			}
 		}
+
 	}
 
 	uint32_t inst = getInstance();
 
-	gs_link_stat_t link = GS_LINK_STAT_UKNOWN;
-	gs_link_stat_t *link_state = &link;
-
 	uint32_t *gs_gui_config = (uint32_t *)*(uint32_t *)((int)inst + 0xe4);
 	uint32_t *gs_info = (uint32_t *)*(uint32_t *)((int)gs_gui_config + 0x4c);
-	gs_modem_get_link_state_wrap = (void *)*(uint32_t *)((int)gs_gui_config + 0x228); //fptr @gs_gui_config + 0x228
+	gs_get_uav_hardware_version = (void *)*(uint32_t *)((int)gs_gui_config + 0x3a8);
+	
+	fpv_lite = is_fpv_lite(gs_info);
 
-	gs_modem_get_link_state_wrap(gs_info, link_state);
+	if(!fpv_lite){
+		gs_modem_get_link_state_wrap = (void *)*(uint32_t *)((int)gs_gui_config + 0x228);
+		gs_enable_audio_liveview = (void *)*(uint32_t *)((int)gs_gui_config + 0x3e0);
+		
+		gs_link_stat_t link = GS_LINK_STAT_UKNOWN;
+		gs_link_stat_t *link_state = &link;
+		gs_modem_get_link_state_wrap(gs_info, link_state);
 
-	if(link != GS_LINK_STAT_NORMAL){
-		clock_gettime(CLOCK_MONOTONIC, &start);
+		if(link != GS_LINK_STAT_NORMAL){
+			clock_gettime(CLOCK_MONOTONIC, &start);
+		}
+
+		clock_gettime(CLOCK_MONOTONIC, &now);
+
+		if((link == GS_LINK_STAT_NORMAL) && !restart_lv_audio && ((now.tv_sec - start.tv_sec) > 9)){
+			restart_lv_audio = true;
+			gs_enable_audio_liveview(gs_info, true);
+			printf("setAudioLiveViewEnable ENABLED\n");
+		}
+		else if((link == GS_LINK_STAT_LOST) && restart_lv_audio){
+			restart_lv_audio = false;
+			gs_enable_audio_liveview(gs_info, false);
+			printf("setAudioLiveViewEnable DISABLED\n");
+		}
 	}
-
-	clock_gettime(CLOCK_MONOTONIC, &now);
-
-	if((link == GS_LINK_STAT_NORMAL) && !restart_lv_audio && ((now.tv_sec - start.tv_sec) > 9)){
-		restart_lv_audio = true;
-		setAudioLiveViewEnable((void *)inst, true, false);
-		printf("setAudioLiveViewEnable ENABLED\n");
-	}
-	else if((link == GS_LINK_STAT_LOST) && restart_lv_audio){
-		restart_lv_audio = false;
-		setAudioLiveViewEnable((void *)inst, false, false);
-		printf("setAudioLiveViewEnable DISABLED\n");
-	}
-
 
 	ret = timeOut(this);
 
--- jni/enable_live_audio.h
+++ jni/enable_live_audio.h
@@ -13,3 +13,7 @@ static int32_t (* gs_modem_get_link_state_wrap)(void *, enum gs_link_stat_t *) =
 static uint32_t (* timeOut)(void *this) = 0;
 static uint32_t (* getInstance)() = 0;
 struct timespec start, now;
+static uint32_t (*gs_enable_audio_liveview)(void *_gs_info, bool b_enable) = 0;
+static uint32_t (*gs_get_uav_hardware_version)(void *_gs_info, char *_hw_ver) = 0;
+bool is_fpv_lite(void *gs_info);
+bool fpv_lite = true;
